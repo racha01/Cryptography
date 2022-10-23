7c34efa0c64803f1d6c3115133ba84299f46ac6a
racha01 <70879490+racha01@users.noreply.github.com>
2022-10-24 00:21:07 +0700

    first commit

---
+++ rsa_aes_des.py
@@ -0,0 +1,122 @@
+import rsa
+
+def generate_keys():
+    (pubKey, privKey) = rsa.newkeys(1024)
+    with open('keys/pubkey.pem', 'wb') as f:
+        f.write(pubKey.save_pkcs1('PEM'))
+
+    with open('keys/privkey.pem', 'wb') as f:
+        f.write(privKey.save_pkcs1('PEM'))
+
+def load_keys():
+    with open('keys/pubkey.pem', 'rb') as f:
+        pubKey = rsa.PublicKey.load_pkcs1(f.read())
+
+    with open('keys/privkey.pem', 'rb') as f:
+        privKey = rsa.PrivateKey.load_pkcs1(f.read())
+
+    return pubKey, privKey
+
+def encrypt(msg, key):
+    return rsa.encrypt(msg.encode('ascii'), key)
+
+def decrypt(ciphertext, key):
+    try:
+        return rsa.decrypt(ciphertext, key).decode('ascii')
+    except:
+        return False
+
+def sign_sha1(msg, key):
+    return rsa.sign(msg.encode('ascii'), key, 'SHA-1')
+
+def verify_sha1(msg, signature, key):
+    try:
+        return rsa.verify(msg.encode('ascii'), signature, key) == 'SHA-1'
+    except:
+        return False
+
+generate_keys()
+pubKey, privKey = load_keys()
+
+message = input('Enter a message:')
+ciphertext = encrypt(message, pubKey)
+
+signature = sign_sha1(message, privKey)
+
+plaintext = decrypt(ciphertext, privKey)
+
+print(f'Cipher text: {ciphertext}')
+print(f'Signature: {signature}')
+
+if plaintext:
+    print(f'Plain text: {plaintext}')
+else:
+    print('Could not decrypt the message.')
+
+if verify_sha1(plaintext, signature, pubKey):
+    print('Signature verified!')
+else:
+    print('Could not verify the message signature.')
+
+
+from Crypto.Cipher import DES
+from secrets import token_bytes
+
+key = token_bytes(8)
+
+def encrypt(msg):
+    cipher = DES.new(key, DES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(msg.encode('ascii'))
+    return nonce, ciphertext, tag
+
+def decrypt(nonce, ciphertext, tag):
+    cipher = DES.new(key, DES.MODE_EAX, nonce=nonce)
+    plaintext = cipher.decrypt(ciphertext)
+
+    try:
+        cipher.verify(tag)
+        return plaintext.decode('ascii')
+    except:
+        return False
+
+
+nonce, ciphertext, tag = encrypt(input('Enter a message: '))
+plaintext = decrypt(nonce, ciphertext, tag)
+print(f'Cipher text: {ciphertext}')
+
+if not plaintext:
+    print('Message is corrupted!')
+else:
+    print(f'Plain text: {plaintext}')
+
+
+from Crypto.Cipher import AES
+from secrets import token_bytes
+
+key = token_bytes(16)
+
+def encrypt(msg):
+    cipher = AES.new(key, AES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(msg.encode('ascii'))
+    return nonce, ciphertext, tag
+
+def decrypt(nonce, ciphertext, tag):
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    plaintext = cipher.decrypt(ciphertext)
+    try:
+        cipher.verify(tag)
+        return plaintext.decode('ascii')
+    except:
+        return False
+
+nonce, ciphertext, tag = encrypt(input('Enter a message: '))
+plaintext = decrypt(nonce, ciphertext, tag)
+print(f'Cipher text: {ciphertext}')
+if not plaintext:
+    print('Message is corrupted')
+else:
+    print(f'Plain text: {plaintext}')
+    
+
